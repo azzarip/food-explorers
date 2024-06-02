bbe3be94e3160a33aac6dcec2e49569089e9aa89
Azzarip <paride21azzari@gmail.com>
2024-06-02 22:18:17 +0200

    installed teavel

---
+++ database/migrations/2024_06_02_215840_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+       Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique()->index();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique()->index()->nullable();
+            $table->string('phone')->unique()->index()->nullable();
+            $table->string('meetup_id')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->timestamp('privacy_at')->nullable();
+            $table->timestamp('marketing_at')->nullable();
+            $table->foreignId('referrer_id')->nullable();
+            $table->boolean('is_female')->nullable();
+            $table->date('birthday')->nullable();
+            $table->foreignId('shipping_id')->nullable();
+            $table->foreignId('billing_id')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+        public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
