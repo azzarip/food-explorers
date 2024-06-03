8f948368849cf52ca825b3f571cdd83e8b67b094
Azzarip <paride21azzari@gmail.com>
2024-06-03 19:43:31 +0200

    contact has meetup

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\Meetup;
+use Illuminate\Auth\Authenticatable;
 use Azzarip\Teavel\Models\Contact as BaseContact;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
 class Contact extends BaseContact
 {
     protected $guarded = [];
 
+    public function meetups()
+    {
+        return $this->belongsToMany(Meetup::class)->withTimestamps();
+    }
 }
