bebe0dc6cd0e7e262e37551ac7e8119ee766e8a0
Azzarip <paride21azzari@gmail.com>
2024-09-30 16:39:31 +0200

    it can open and close

--- domains/base/routes/base-routes.php
+++ domains/base/routes/base-routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Session;
@@ -13,7 +14,24 @@ Route::group([
     Route::get('/', fn () => 'Website under construction');
 
 
+    Route::get('/start', function () {
+        $directory = storage_path('to_delete');
+        File::makeDirectory($directory, 0755, true);
+
+        return 'ok';
+    });
+
+    Route::get('/stop', function () {
+        $directory = storage_path('to_delete');
+        File::deleteDirectory($directory, 0755);
+    });
+    
     Route::get('/yu-an-omakase', function () {
+        $directory = storage_path('to_delete');
+
+        if (!File::exists($directory)) {
+            return redirect('/closed');
+        }
         $targetDateTime = Carbon::create(2024, 10, 04, 12, 00, 0);
 
         if(now()->isAfter($targetDateTime)) {
--- resources/views/closed.blade.php
+++ resources/views/closed.blade.php
@@ -16,6 +16,19 @@
     <h1 class="font-serif text-4xl font-semibold text-center">Omakase at Yu-An</h1>
     <p class="mt-2 text-xl text-center">Tuesday 15. October 2024, 19:00</p>
 
-    <p class="mt-8 text-6xl text-center">Bookings are closed!</p>
+    <p class="py-4 mt-8 text-6xl text-center border-4 border-black rounded-md">Bookings are closed!</p>
+
+    <h3 class="block mt-12 text-xl font-semibold">The Fine Print Just to Be Totally Clear</h3>
+    <ul class="pt-2 mb-4 space-y-4 list-inside">
+        <li>All prices are in Swiss Francs and include the MwSt.</li>
+        <li>Spots are assigned on a first-come, first-served basis. They may terminate before the deadline.</li>
+        <li>The <strong>deadline</strong> for the booking, cancellations and reimbursement is:</li>
+        <li class="text-xl font-semibold text-center">Friday 04 October 2024 12:00pm (noon), Zurich Time</li>
+        <li>To cancel, write a message via Whatsapp (preferably), or Meetup, the timestamp of the message is the reference. I will twint you back the total amount or cancel the credit cart payment.</li>
+        <li class="font-semibold">Changing the RSVP on Meetup will not be considered, since I do not get notified. Please write me.</li>
+        <li>Any later from that date, no reimbursement will be issued, and the transaction will be considered final.</li>
+        <li>The participation is not nominative, in case you cannot join, you can give your place to someone else. (You have to find that person.)</li>
+        <li>In case of cancellation of the dinner from my side, a full 100% reimbursement will be issued.</li>
+    </ul>
 </article>
 @endsection
