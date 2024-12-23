1dd0dc266f8e68bb1fc1b0d01580cda28c57e08f
Azzarip <paride21azzari@gmail.com>
2024-12-23 21:22:56 +0100

    fixed size width of form

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 @section('main')
 <div class="flex justify-center">
 <div class="w-full max-w-3xl my-2 bg-white border-yellow-400 lg:my-8 border-y-8 rounded-xl">
-    <div class="max-w-lg px-4 mx-auto mt-2">
+    <div class="max-w-sm px-4 mx-auto mt-2">
         @yield('content')
     </div>
 </div>
