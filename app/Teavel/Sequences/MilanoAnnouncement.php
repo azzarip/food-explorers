7cccd4b2fec74835073f89777a10954a8ec44407
Azzarip <paride21azzari@gmail.com>
2025-01-11 18:15:31 +0100

    added milano sequence

---
+++ app/Teavel/Sequences/MilanoAnnouncement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Teavel\Sequences;
+
+use App\Teavel\Emails\MilanoAnnouncement as EmailsMilanoAnnouncement;
+use Azzarip\Teavel\Automations\Wait;
+use Azzarip\Teavel\Automations\SequenceAutomation;
+
+class MilanoAnnouncement extends SequenceAutomation
+{
+    public function start()
+    {
+        return Wait::until('11.01.2025 19:00')
+            ->then('sendAnnouncement')
+            ->precise();
+    }
+
+    public function sendAnnouncement() {
+        $this->email(EmailsMilanoAnnouncement::class);
+    }
+}
