b94709ad827f8e2c8fc0c98a09a7d9c016ac2f72
Azzarip <paride21azzari@gmail.com>
2025-01-24 17:19:30 +0100

    boh commit

--- app/Actions/Event/CancelParticipation.php
+++ app/Actions/Event/CancelParticipation.php
@@ -10,30 +10,26 @@ class CancelParticipation {
 
     public static function cancel(Contact $contact, Event $event)
     {
-        $pivot = DB::table('contact_event')
-        ->where('event_id', $event->id)
-        ->where('contact_id', auth()->user()->id)
-        ->first();
-
-
         $contact->events()->updateExistingPivot($event->id, [
             'deleted_at' => now(),
             'queue' => null,
         ]); 
 
-        if($pivot->queue === null) {
-            $new = DB::table('contact_event')
+        $new = DB::table('contact_event')
             ->where('event_id', $event->id)
             ->whereNull('deleted_at')
             ->whereNotNull('queue')
             ->orderBy('queue')
             ->first();
-            if($new) {
-                $event->contacts()->updateExistingPivot($new->contact_id, [
-                    'queue' => null,
-                ]); 
-            }
+        
+        if(empty($new)) {
+            return;
         }
 
+        $event->contacts()->updateExistingPivot($new->contact_id, [
+            'queue' => null,
+        ]); 
+
+        
     }
 }
