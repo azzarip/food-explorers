964c9ad28f5cf1acccaa25317deab43c11f1ce56
Azzarip <paride21azzari@gmail.com>
2025-01-26 17:48:12 +0100

    fixed redirect

--- domains/book/resources/views/components/auth/login.blade.php
+++ domains/book/resources/views/components/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 <div class="mt-8">
 <p class="mb-1 text-xl">Are you new?</p>
-<a href="{{ route('login') }}"
+<a href="{{ request()->url() }}"
     class="block w-full max-w-lg py-1 mx-auto text-2xl text-center border-4 border-yellow-500 rounded-full hover:bg-yellow-500">
     Become a Food Explorer <x-heroicon-o-arrow-right class="inline w-6 mb-1 aspect-auto" />
 </a>
