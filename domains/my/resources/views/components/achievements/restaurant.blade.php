e82379e2fd0d29bea2882ddec0954e1c3eb1ebc3
Azzarip <paride21azzari@gmail.com>
2025-03-03 00:14:56 +0100

    improved achievemtns

--- domains/my/resources/views/components/achievements/restaurant.blade.php
+++ domains/my/resources/views/components/achievements/restaurant.blade.php
@@ -6,19 +6,21 @@
         <div class="relative min-w-[80px] aspect-square py-2 ml-4 h-full my-auto">
             <img src='{{ image("achievements\\$key.webp") }}' alt="{{ $key }} logo" width="60px" height="60px" class="absolute inset-0 w-full h-full">
         </div>
-        <div class="w-full h-full pr-1">
+        <div class="w-full h-full lg:pr-1">
     
+            <div class="mx-auto text-sm font-semibold sm:float-right w-fit">
             @if($active)
-            <p class="p-1 mr-1 text-sm font-semibold rounded-md sm:float-right md:order-3 w-fit text-serif text-amber-800 "><x-heroicon-s-sparkles class="inline w-4 mb-1 aspect-square" /> EXPLORED <x-heroicon-s-sparkles class="inline w-4 mb-1 aspect-square" /></p>
+            <p class="p-1 rounded-md text-amber-800"><x-heroicon-s-sparkles class="inline w-4 mb-1 aspect-square" /> EXPLORED <x-heroicon-s-sparkles class="inline w-4 mb-1 aspect-square" /></p>
             @elseif($unlocked)
-            <p class="p-1 ml-auto mr-2 text-sm font-semibold text-gray-700 rounded-md sm:float-right md:order-3 w-fit bg-stone-200 text-serif"><x-heroicon-s-lock-open class="inline w-4 mb-1 ml-1 aspect-square" /> AVAILABLE</p>
+            <p class="p-1 text-gray-700 rounded-md bg-stone-200"><x-heroicon-s-lock-open class="inline w-4 mb-1 ml-1 aspect-square" /> AVAILABLE</p>
             @else
-            <p class="p-1 ml-auto mr-2 text-sm font-semibold text-gray-700 rounded-md sm:float-right md:order-3 w-fit bg-stone-200 text-serif"><x-heroicon-s-lock-closed class="inline w-4 mb-1 ml-1 aspect-square" /> LOCKED</p>
+            <p class="p-1 text-gray-700 rounded-md bg-stone-200"><x-heroicon-s-lock-closed class="inline w-4 mb-1 ml-1 aspect-square" /> LOCKED</p>
             @endif
+        </div>
             
             <h3 class="mb-1 text-2xl font-semibold text-center sm:text-left font-head">{{ $title }}</h3>
             
-            <p class="italic @if(! $unlocked)  select-none blur-sm @endif">{{ $description }}</p>
+            <p class="italic max-sm:px-2 @if(! $unlocked)  select-none blur-sm @endif">{{ $description }}</p>
         </div>
     </div>
 
